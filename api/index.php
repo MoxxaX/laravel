b69fbdab3217667cc3fedd696d535632bf1774b1
moxxa <127527125+MoxxaX@users.noreply.github.com>
2026-06-08 15:01:53 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,5 @@
-<?php 
-require __DIR__. '/../public/index.php';
+<?php
+
+putenv('APP_KEY=' . ($_SERVER['APP_KEY'] ?? $_ENV['APP_KEY'] ?? getenv('APP_KEY')));
+
+require __DIR__ . '/../public/index.php';
