8e3d54007bda2693b85bb049b22f3724ea6ec642
MoxxaX <moehanbao123@gmail.com>
2026-06-23 22:43:51 +0700

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -1,67 +1,55 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\MahasiswaController;
 
-Route::middleware('auth')->group(function () {
-    Route::get('/mahasiswa', [MahasiswaController::class, 'index'])
-        ->name('mahasiswa.index');
-
-    Route::get('/mahasiswa/create', [MahasiswaController::class, 'create'])
-        ->name('mahasiswa.create');
-
-    Route::post('/mahasiswa', [MahasiswaController::class, 'store'])
-        ->name('mahasiswa.store');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/prodi', [ProdiController::class, 'index'])
-        ->name('prodi.index');
-
-    Route::get('/prodi/create', [ProdiController::class, 'create'])
-        ->name('prodi.create');
-
-    Route::post('/prodi', [ProdiController::class, 'store'])
-        ->name('prodi.store');
-});
-Route::middleware('auth')->group(function () {
-    Route::get('/fakultas', [FakultasController::class, 'index'])
-        ->name('fakultas.index');
-
-    Route::get('/fakultas/create', [FakultasController::class, 'create'])
-        ->name('fakultas.create');
-
-    Route::post('/fakultas', [FakultasController::class, 'store'])
-        ->name('fakultas.store');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/periode', [PeriodeController::class, 'index'])
-        ->name('periode.index');
-
-    Route::get('/periode/create', [PeriodeController::class, 'create'])
-        ->name('periode.create');
-
-    Route::post('/periode', [PeriodeController::class, 'store'])
-        ->name('periode.store');
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-// AUTH DASHBOARD
 Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth'])
+    ->middleware('auth')
     ->name('dashboard');
 
-// PROFILE ROUTES
 Route::middleware('auth')->group(function () {
+
+    Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+    Route::get('/mahasiswa/create', [MahasiswaController::class, 'create'])->name('mahasiswa.create');
+    Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.store');
+
+    Route::get('/mahasiswa/{id}/edit', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+    Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
+    Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
+
+    Route::get('/prodi', [ProdiController::class, 'index'])->name('prodi.index');
+    Route::get('/prodi/create', [ProdiController::class, 'create'])->name('prodi.create');
+    Route::post('/prodi', [ProdiController::class, 'store'])->name('prodi.store');
+
+    Route::get('/prodi/{id}/edit', [ProdiController::class, 'edit'])->name('prodi.edit');
+    Route::put('/prodi/{id}', [ProdiController::class, 'update'])->name('prodi.update');
+    Route::delete('/prodi/{id}', [ProdiController::class, 'destroy'])->name('prodi.destroy');
+
+    Route::get('/fakultas', [FakultasController::class, 'index'])->name('fakultas.index');
+    Route::get('/fakultas/create', [FakultasController::class, 'create'])->name('fakultas.create');
+    Route::post('/fakultas', [FakultasController::class, 'store'])->name('fakultas.store');
+
+    Route::get('/fakultas/{id}/edit', [FakultasController::class, 'edit'])->name('fakultas.edit');
+    Route::put('/fakultas/{id}', [FakultasController::class, 'update'])->name('fakultas.update');
+    Route::delete('/fakultas/{id}', [FakultasController::class, 'destroy'])->name('fakultas.destroy');
+
+    Route::get('/periode', [PeriodeController::class, 'index'])->name('periode.index');
+    Route::get('/periode/create', [PeriodeController::class, 'create'])->name('periode.create');
+    Route::post('/periode', [PeriodeController::class, 'store'])->name('periode.store');
+
+    Route::get('/periode/{id}/edit', [PeriodeController::class, 'edit'])->name('periode.edit');
+    Route::put('/periode/{id}', [PeriodeController::class, 'update'])->name('periode.update');
+    Route::delete('/periode/{id}', [PeriodeController::class, 'destroy'])->name('periode.destroy');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
